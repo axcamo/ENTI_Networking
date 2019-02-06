90a5dbc7228a6c3dbaec848112eaa92598fdb533
Axel Castells Monllau <axelcastells.dev@gmail.com>
2019-02-06 17:37:03 +0100

    Server simple code.

--- ENTI_Networking/main.cpp
+++ ENTI_Networking/main.cpp
@@ -1,3 +1,22 @@
+#include <SFML/Network.hpp>
+#include <thread>
+
+sf::Socket::Status status;
+std::thread receiver;
 int main() {
+	//status =
+	sf::TcpListener listener;
+	status = listener.listen(50000);
+	sf::TcpSocket sock;
+	status = listener.accept(sock);
+
+	if (status == sf::Socket::Status::Done)
+	{
+		sock.send("Rawr", 4);
+		char buffer[512];
+		size_t br;
+		sock.receive(buffer, 10, br);
+	}
+
 	return 0;
 }
